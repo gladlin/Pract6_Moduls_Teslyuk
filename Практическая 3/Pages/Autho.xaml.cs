24f3e74fdb1a236df043c266b3f903490c1f0e6d
Mickey <artplay1995@gmail.com>
2024-10-05 15:15:56 +0700

    Добавьте файлы проекта.

---
+++ Практическая 3/Pages/Autho.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Практическая_3.Pages;
+
+namespace Практическая_3.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Autho.xaml
+    /// </summary>
+    public partial class Autho : Page
+    {
+        public Autho()
+        {
+            InitializeComponent();
+        }
+
+        private void txtbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnEnterGuests_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Client());
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
