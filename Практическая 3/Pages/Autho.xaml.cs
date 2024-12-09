e7a290abd12947c3544077c687145aa7ad82b35f
gladlin <teslyukalina@yandex.ru>
2024-12-09 15:49:25 +0700

    Pract7 is done

--- Практическая 3/Pages/Autho.xaml.cs
+++ Практическая 3/Pages/Autho.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 using Практическая_3.Models;
 using Практическая_3.Services;
 
@@ -17,6 +19,8 @@ namespace Практическая_3.Pages
             click = 0;
             tbCaptcha.Visibility = Visibility.Hidden;
             tblCaptcha.Visibility = Visibility.Hidden;
+            tbTimer.Visibility = Visibility.Hidden;
+            tbLeftTime.Visibility = Visibility.Hidden;
         }
 
         private void btnEnterGuests_Click(object sender, RoutedEventArgs e)
@@ -26,35 +30,42 @@ namespace Практическая_3.Pages
 
         private void GenerateCaptcha()
         {
+            click++;
             tblCaptcha.Text = CaptchaGenerator.GenerateCaptchaText(6);
             tblCaptcha.TextDecorations = TextDecorations.Strikethrough;
             tbCaptcha.Visibility = Visibility.Visible;
             tblCaptcha.Visibility = Visibility.Visible;
         }
-
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
-            click++;
             string login = tbLogin.Text.Trim();
             string password = Hash.HashPassword(tbPassword.Password.Trim());
 
             MusicRecordEntities db = Helper.GetContext();
             var user = db.UserAccounts .FirstOrDefault(x => x.username == login && x.password == password);
 
-            if (click == 1)
+
+            if (click <= 1)
             {
                 if (user == null)
                 {
                     MessageBox.Show("Вы ввели логин или пароль неверно!");
                     tbPassword.Clear();
                     tbLogin.Clear();
+                    GenerateCaptcha();
                 }
                 else
                 {
-                    GenerateCaptcha();
+                    string roleName = user.Roles.role_name;
+                    MessageBox.Show($"Вы вошли под: {roleName}");
+                    tbLogin.Clear();
+                    tbPassword.Clear();
+                    tbCaptcha.Clear();
+                    LoadPage(user);
+
                 }
             }
-            else if (click > 1)
+            else if (click == 2)
             {
                 if (user != null && tbCaptcha.Text.Trim() == tblCaptcha.Text)
                 {
@@ -74,7 +85,54 @@ namespace Практическая_3.Pages
                     GenerateCaptcha();
                 }
             }
+            else if(click == 3)
+            {
+                MessageBox.Show("Вы три раза неверно ввели данные!");
+                click = 0;
+                btnEnter.IsEnabled = false;
+                btnEnterGuests.IsEnabled = false;
+                StartTimer();
+            }
         }
+
+        private int remainingSeconds = 10;
+        private DispatcherTimer timer;
+        private void StartTimer()
+        {
+            remainingSeconds = 10;
+            tbCaptcha.Clear();
+            tbCaptcha.Visibility = Visibility.Hidden;
+            tblCaptcha.Visibility = Visibility.Hidden;
+            tbPassword.Clear();
+            tbLogin.Clear();
+            tbPassword.IsEnabled = false;
+            tbLogin.IsEnabled = false;
+            tbTimer.Visibility = Visibility.Visible;
+            tbLeftTime.Visibility = Visibility.Visible;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tbTimer.Text = remainingSeconds.ToString();
+            remainingSeconds--;
+            if (remainingSeconds < 0)
+            {
+                timer.Stop();
+                tbTimer.Text = "";
+                tbPassword.IsEnabled = true;
+                tbLogin.IsEnabled = true;
+                tbTimer.Visibility = Visibility.Hidden;
+                tbLeftTime.Visibility = Visibility.Hidden;
+                btnEnter.IsEnabled = true;
+                btnEnterGuests.IsEnabled = true;
+            }
+        }
+
 
         private void LoadPage(UserAccounts user)
         {
@@ -92,6 +150,11 @@ namespace Практическая_3.Pages
                     MessageBox.Show("Неизвестная роль пользователя.");
                     break;
             }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
